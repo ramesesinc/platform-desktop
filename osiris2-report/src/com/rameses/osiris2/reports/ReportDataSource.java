d62216149d1a06d09c168097a44d02dca3d093d6
worgflores <worgflores@gmail.com>
2020-08-05 12:04:47 +0800

    osiris2-report: updated

--- osiris2-report/src/com/rameses/osiris2/reports/ReportDataSource.java
+++ osiris2-report/src/com/rameses/osiris2/reports/ReportDataSource.java
@@ -5,11 +5,16 @@ import com.rameses.common.PropertyResolver;
 import com.rameses.util.Base64Cipher;
 import com.rameses.util.QRCodeUtil;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-import javax.swing.JOptionPane;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRField;
 import net.sf.jasperreports.engine.JRRewindableDataSource;
@@ -68,48 +73,120 @@ public class ReportDataSource implements JRRewindableDataSource {
     //added in getFieldValue. If value is byte[] then we need to convert this to InputStream.
     //This is very common in images.
     public Object getFieldValue(JRField jRField) throws JRException {
-        Object field = null;
+        Object value = null;
         String fieldName = null;
         try {
             fieldName = jRField.getName();
             if ( "_source".equals( fieldName )) {
                 return new ReportDataSource( getSource());
             }
-            field = propertyResolver.getProperty(currentObject, fieldName);
-            if(field==null) {
+            
+            value = propertyResolver.getProperty(currentObject, fieldName);
+            if ( value == null ) {
                 return null;
             }
-            if( field.getClass() == byte[].class ) {
-                return new ByteArrayInputStream( (byte[])field);
+            if (value.getClass() == byte[].class ) {
+                return new ByteArrayInputStream((byte[]) value);
             }
-            else if( field.getClass() == String.class ) {
-                //check if it has qrcode or a base64;
-                String str = (String)field;
-                if( str.startsWith("qrcode:")) {
-                    String newstr = str.substring(7);
-                    Object z =  QRCodeUtil.generateQRCode(newstr);
-                    return new ByteArrayInputStream( QRCodeUtil.generateQRCode(newstr));
+            else if( value.getClass() == String.class ) {
+                
+                if ( java.util.Date.class.isAssignableFrom( jRField.getValueClass())) {
+                    return convertDateObject( jRField.getValueClass(), value.toString() ); 
                 }
-                else if( str.startsWith("image:")) {
-                    String newstr = str.substring(6);
-                    JOptionPane.showMessageDialog( null, "str is " + newstr);
-                    System.out.println( newstr );
-                    Object bytes = (new Base64Cipher()).decode(newstr);
-                    return new ByteArrayInputStream( (byte[]) bytes );
+                
+                String str = value.toString(); 
+                if ( str.startsWith("qrcode:")) {
+                    String newstr = str.substring(7);
+                    byte[] bytes =  QRCodeUtil.generateQRCode( newstr );
+                    return new ByteArrayInputStream( bytes );
+                }
+                else if ( str.startsWith("image:")) {
+                    String[] arr = str.substring(6).split(",");
+                    String encstr = (arr.length > 1 ? arr[1] : arr[0]); 
+                    String type = (arr.length > 1 ? arr[0] : ""); 
+                    
+                    Object obj = new Base64Cipher().decode( encstr, false );
+                    byte[] bytes = (byte[]) obj;
+                    if ( "url".equalsIgnoreCase( type )) { 
+                        BytesHandler handler = new BytesHandler( bytes ); 
+                        return new URL( null, "bytes:///", handler );
+                    } 
+                    else {
+                        return new ByteArrayInputStream( bytes );
+                    }
                 }
                 else  {
                     return str;
                 }
             }
             else if( jRField.getValueClass().isAssignableFrom( Collection.class) ) {
-                return new ReportDataSource( field );
+                return new ReportDataSource( value );
             } 
             else { 
-                return field;
+                return value;
             }
-        } catch(Exception ex) {
+        } 
+        catch(Throwable ex) {
             System.out.println("Error on field [" + fieldName  + "] caused by " + ex.getMessage());
+            ex.printStackTrace();
             return null; 
         } 
     }  
+    
+    
+    private SimpleDateFormat YMD = new SimpleDateFormat("yyyy-MM-dd"); 
+    private SimpleDateFormat YMD_HMS = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
+    
+    private Object convertDateObject( Class targetClass, String value ) { 
+        try {
+            return new java.sql.Timestamp( YMD_HMS.parse( value ).getTime());
+        } catch (ParseException ex) {
+            //do nothing 
+        }
+        try {
+            return new java.sql.Date( YMD.parse(value).getTime() ); 
+        } catch (ParseException ex) {
+            ex.printStackTrace(); 
+            return null; 
+        }
+    }
+    
+    private class BytesHandler extends java.net.URLStreamHandler {
+
+        private byte[] bytes; 
+        
+        public BytesHandler( byte[] bytes ) {
+            this.bytes = bytes; 
+        }
+        
+        protected URLConnection openConnection(URL u) throws IOException {
+            BytesConnection bc = new BytesConnection(u); 
+            bc.handler = this; 
+            return bc; 
+        }
+
+        public String getProtocol() {
+            return "bytes";
+        }
+
+        public URL getResource(String spath) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
+    
+    private class BytesConnection extends URLConnection {
+        
+        private BytesHandler handler;
+        
+        public BytesConnection(URL url) {
+            super(url);
+        }
+
+        public void connect() throws IOException {
+        }
+
+        public InputStream getInputStream() throws IOException {
+            return new ByteArrayInputStream( handler.bytes ); 
+        }
+    }    
 }
